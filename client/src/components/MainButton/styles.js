aa057c97f4120c7985f70a6b2ecbbd6b6b2e80ae
Matvey Nurminen <mat.nurminen@hotmail.com>
2021-09-20 08:20:56 +0300

    redux and start front-end

--- client/src/components/MainButton/styles.js
+++
@@ -1,9 +0,0 @@
-import { withStyles } from '@material-ui/core/styles';
-
-const styles = (theme) => ({
-  '& > *': {
-    margin: theme.spacing(1),
-  },
-});
-
-export default (component) => withStyles(styles)(component);
